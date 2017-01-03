48e3c5c3f46360cdbb271a491ac508ff1c5fe85e
Zach G <zeg2@rice.edu>
2017-01-03 13:28:35 -0600

    adds icon and background.js; implements message passing; logs link in console on click

---
+++ background.js
@@ -0,0 +1,9 @@
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // Send message to active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    var activeTab = tabs[0];
+    // alert(activeTab);
+    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  });
+});
--- content.js
+++ content.js
@@ -1,3 +1,9 @@
-var firstHref = $("a[href^='http']").eq(0).attr("href");
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.message === "clicked_browser_action") {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
 
-console.log(firstHref);
+      console.log(firstHref);
+    }
+  }
+);
