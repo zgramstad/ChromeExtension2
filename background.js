6232296ef023e0ecbff7134aa00eeba6b0589044
Zach G <zeg2@rice.edu>
2017-01-03 13:37:48 -0600

    adds opening new tab with link

--- background.js
+++ background.js
@@ -3,7 +3,16 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   // Send message to active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
     var activeTab = tabs[0];
-    // alert(activeTab);
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
 });
+
+// Called when message received
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.message === "open_new_tab") {
+      // Open new tab with url
+      chrome.tabs.create({"url": request.url});
+    }
+  }
+);
